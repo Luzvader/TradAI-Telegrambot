2f9ee4146560737cf8f3a499016d56f9d2ec3b49
José Martínez Montero <58337314+Luzvader@users.noreply.github.com>
2025-07-27 18:27:34 +0200

    Add balance helpers and fix engine

--- tradai/indicators.py
+++ tradai/indicators.py
@@ -185,7 +185,7 @@ def detect_candle(
 
         # Patrón de martillo
         c1, o1, h1, l1 = cl[-2], op[-2], h[-2], l[-2]
-        c2, o2, h2, l2 = cl[-1], op[-1], h[-1], l[-1]]
+        c2, o2, h2, l2 = cl[-1], op[-1], h[-1], l[-1]
 
         if c1 < o1 and c2 > o2 and (c2 - o2) < (o1 - c1) * 0.5:
             return "Hammer"
